dd45ebbf373af27f34eeb6fa7a12b4ceb0407292
Yoseph Amare <yosephamare@ad.doe.carleton.ca>
2026-05-15 15:00:43 -0400

    Added testbench for spi master to root sim folder

---
+++ sim/tb_spi_master.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 05/15/2026 02:56:06 PM
+// Design Name: 
+// Module Name: tb_spi_master
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module tb_spi_master(
+
+    );
+endmodule
